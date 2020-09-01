4d8db5274cf3a245de8a19c43309b6671142e840
kemora08 <kevin_mora89@hotmail.com>
2020-08-31 20:15:52 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Beer
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.12.
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class APIService {
+
+  constructor(private httpService: HttpClient) { }
+
+  async get(){
+
+  }
+}
