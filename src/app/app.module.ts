46ab3f73e05047eaf40590a78da1b0bc516eb1c9
kemora08 <kevin_mora89@hotmail.com>
2020-09-05 15:58:46 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule} from '@angular/material/table';
+import { MatSortModule} from '@angular/material/sort';
+import { MatPaginatorModule} from '@angular/material/paginator';
+import { MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
